f561c5ac46507c862cd2905b08202623c858fc9b
Rasel Hasan (Senior Software Engineer & Manager) <36207679+RaselHasanSwe@users.noreply.github.com>
2026-07-06 11:01:34 +0100

    cors config

--- config/cors.php
+++ config/cors.php
@@ -22,6 +22,7 @@ return [
     'allowed_origins' => [
         'http://localhost:3000',
         'http://127.0.0.1:3000',
+        'http://206.189.87.32',
     ],
 
     'allowed_origins_patterns' => [],
